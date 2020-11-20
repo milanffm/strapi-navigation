6a1168e4ca69285a9e2ed7a603f43c8d2f9ec3a7
Milan Peters <m.peters@graphicon.de>
2020-11-20 16:26:27 +0100

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'baece9891bf53685e33e0199c6629e5a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9008afff-ebbe-404a-9b3d-15cff74370ad'
+};
